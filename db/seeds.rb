31873c438da944a374573f696c2dfae15eec23c2
Dainel Vera <arubinofaux@gmail.com>
2021-02-20 18:18:44 -0700

    bump it

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-baseNumber = 53520000
+baseNumber = 53000000
 maxNumber = 53539999
 
 while TRUE
